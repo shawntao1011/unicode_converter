4254c7911566eae150edcadd6aadfbdba2e3bc53
tao.sun <tao.sun@capitaledge.cn>
2021-05-17 18:42:40 +0800

    start working on cmake google test

--- boost/convert_func/src/converter.cpp
+++ boost/convert_func/src/converter.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <boost/locale/encoding.hpp>
 
 namespace utilities{
@@ -8,14 +7,3 @@ namespace utilities{
 }
 
 constexpr auto converter=utilities::boost_converter;
-
-int main(){
-    std::string temp="中";
-    std::cout<<"sample chinese char is "<<temp<<std::endl;
-
-    auto temp_utf8 = converter(temp,"GBK","UTF-8");
-
-    auto temp_gbk = converter(temp_utf8,"UTF-8","GBK");
-    std::cout<<"after conver: "<<temp_gbk<<std::endl;
-    return 0;
-}
---
+++ boost/gbk_utf_sample/build/CMakeFiles/mytest.dir/utf_gbk_sample.cpp.o.d
@@ -0,0 +1,158 @@
+CMakeFiles/mytest.dir/utf_gbk_sample.cpp.o: \
+ /cygdrive/e/unicode_converter/boost/gbk_utf_sample/utf_gbk_sample.cpp \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/iostream \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/i686-pc-cygwin/bits/c++config.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/i686-pc-cygwin/bits/os_defines.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/i686-pc-cygwin/bits/cpu_defines.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/ostream \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/ios \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/iosfwd \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/stringfwd.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/memoryfwd.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/postypes.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/cwchar /usr/include/wchar.h \
+ /usr/include/_ansi.h /usr/include/newlib.h \
+ /usr/include/_newlib_version.h /usr/include/sys/config.h \
+ /usr/include/machine/ieeefp.h /usr/include/sys/features.h \
+ /usr/include/cygwin/config.h /usr/include/sys/reent.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/stddef.h \
+ /usr/include/sys/_types.h /usr/include/machine/_types.h \
+ /usr/include/machine/_default_types.h /usr/include/sys/lock.h \
+ /usr/include/sys/cdefs.h /usr/lib/gcc/i686-pc-cygwin/10/include/stdarg.h \
+ /usr/include/sys/_locale.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/exception \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/exception.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/exception_ptr.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/exception_defines.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/cxxabi_init_exception.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/typeinfo \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/hash_bytes.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/new \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/nested_exception.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/move.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/type_traits \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/char_traits.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/stl_algobase.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/functexcept.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/cpp_type_traits.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/ext/type_traits.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/ext/numeric_traits.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/stl_pair.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/stl_iterator_base_types.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/stl_iterator_base_funcs.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/concept_check.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/debug/assertions.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/stl_iterator.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/ptr_traits.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/debug/debug.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/predefined_ops.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/cstdint \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/stdint.h /usr/include/stdint.h \
+ /usr/include/sys/_intsup.h /usr/include/sys/_stdint.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/localefwd.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/i686-pc-cygwin/bits/c++locale.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/clocale /usr/include/locale.h \
+ /usr/include/_ansi.h /usr/lib/gcc/i686-pc-cygwin/10/include/c++/cctype \
+ /usr/include/ctype.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/ios_base.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/ext/atomicity.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/i686-pc-cygwin/bits/gthr.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/i686-pc-cygwin/bits/gthr-default.h \
+ /usr/include/pthread.h /usr/include/sys/types.h \
+ /usr/include/machine/endian.h /usr/include/machine/_endian.h \
+ /usr/include/bits/endian.h /usr/include/sys/select.h \
+ /usr/include/sys/_sigset.h /usr/include/sys/_timeval.h \
+ /usr/include/sys/timespec.h /usr/include/sys/_timespec.h \
+ /usr/include/sys/_pthreadtypes.h /usr/include/sys/cpuset.h \
+ /usr/include/machine/types.h /usr/include/endian.h \
+ /usr/include/bits/byteswap.h /usr/include/bits/wordsize.h \
+ /usr/include/sys/sysmacros.h /usr/include/signal.h \
+ /usr/include/sys/signal.h /usr/include/cygwin/signal.h \
+ /usr/include/sys/ucontext.h /usr/include/sched.h \
+ /usr/include/sys/sched.h /usr/include/time.h /usr/include/machine/time.h \
+ /usr/include/cygwin/time.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/i686-pc-cygwin/bits/atomic_word.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/locale_classes.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/string \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/allocator.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/i686-pc-cygwin/bits/c++allocator.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/ext/new_allocator.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/ostream_insert.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/cxxabi_forced.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/stl_function.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/backward/binders.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/range_access.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/initializer_list \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/iterator_concepts.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/concepts \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/range_cmp.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/int_limits.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/basic_string.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/ext/alloc_traits.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/alloc_traits.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/stl_construct.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/ext/string_conversions.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/cstdlib /usr/include/stdlib.h \
+ /usr/include/machine/stdlib.h /usr/include/alloca.h \
+ /usr/include/cygwin/stdlib.h /usr/include/cygwin/wait.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/std_abs.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/cstdio /usr/include/stdio.h \
+ /usr/include/sys/stdio.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/cerrno /usr/include/errno.h \
+ /usr/include/sys/errno.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/charconv.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/functional_hash.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/basic_string.tcc \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/locale_classes.tcc \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/system_error \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/i686-pc-cygwin/bits/error_constants.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/stdexcept \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/streambuf \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/streambuf.tcc \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/basic_ios.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/locale_facets.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/cwctype /usr/include/wctype.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/i686-pc-cygwin/bits/ctype_base.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/streambuf_iterator.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/i686-pc-cygwin/bits/ctype_inline.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/locale_facets.tcc \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/basic_ios.tcc \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/ostream.tcc \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/istream \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/istream.tcc \
+ /usr/include/boost/locale/encoding.hpp \
+ /usr/include/boost/locale/config.hpp \
+ /usr/include/boost/locale/definitions.hpp /usr/include/boost/config.hpp \
+ /usr/include/boost/config/user.hpp \
+ /usr/include/boost/config/detail/select_compiler_config.hpp \
+ /usr/include/boost/config/compiler/gcc.hpp \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/cstddef \
+ /usr/include/boost/config/detail/select_stdlib_config.hpp \
+ /usr/include/boost/config/stdlib/libstdcpp3.hpp /usr/include/unistd.h \
+ /usr/include/sys/unistd.h /usr/include/getopt.h \
+ /usr/include/boost/config/detail/select_platform_config.hpp \
+ /usr/include/boost/config/platform/cygwin.hpp \
+ /usr/include/boost/config/detail/posix_features.hpp \
+ /usr/include/boost/config/detail/suffix.hpp \
+ /usr/include/boost/config/auto_link.hpp \
+ /usr/include/boost/locale/info.hpp \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/locale \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/locale_facets_nonio.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/ctime \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/i686-pc-cygwin/bits/time_members.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/i686-pc-cygwin/bits/messages_members.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/codecvt.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/locale_facets_nonio.tcc \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/locale_conv.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/unique_ptr.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/utility \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/stl_relops.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/tuple \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/array \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/uses_allocator.h \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/invoke.h \
+ /usr/include/boost/locale/encoding_errors.hpp \
+ /usr/include/boost/locale/encoding_utf.hpp \
+ /usr/include/boost/locale/utf.hpp /usr/include/boost/cstdint.hpp \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/iterator \
+ /usr/lib/gcc/i686-pc-cygwin/10/include/c++/bits/stream_iterator.h
