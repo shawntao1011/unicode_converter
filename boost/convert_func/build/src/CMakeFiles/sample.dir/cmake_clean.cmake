87c9475114528bc599c618b27586ad5c4fb1b70e
tao.sun <tao.sun@capitaledge.cn>
2021-05-18 14:07:27 +0800

    add boost converter case

---
+++ boost/convert_func/build/src/CMakeFiles/sample.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sample.dir/sample.cpp.o"
+  "CMakeFiles/sample.dir/sample.cpp.o.d"
+  "libsample.dll.a"
+  "sample.exe"
+  "sample.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ boost/convert_func/src/sample.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "converter.hpp"
+
+int main(){
+    std::string temp="中";
+    std::cout<<"sample chinese char is "<<temp<<std::endl;
+
+    auto temp_utf8 = converter(temp,"GBK","UTF-8");
+
+    auto temp_gbk = converter(temp_utf8,"UTF-8","GBK");
+    std::cout<<"after conver: "<<temp_gbk<<std::endl;
+    return 0;
+}
