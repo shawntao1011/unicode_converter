bbc21b5fc4f27aaca8a3db0b6853ffcb2f665c3c
tao.sun <tao.sun@capitaledge.cn>
2021-05-24 18:27:18 +0800

    the gbk_utf_sample now uses a header_only lib.
    Lib is decoupled from exe.

---
+++ boost/gbk_utf_sample/lib/include/converter.hpp
@@ -0,0 +1,9 @@
+#include <boost/locale/encoding.hpp>
+
+namespace utilities{
+    inline std::string boost_converter(std::string const& text,std::string const&to_encoding,std::string const&from_encoding){
+        return boost::locale::conv::between(text,to_encoding,from_encoding);
+    }
+}
+
+const auto converter=utilities::boost_converter;
