68d4553f2bf65e115b534ce5a7cc3e07a094145b
tao.sun <tao.sun@capitaledge.cn>
2021-05-26 16:16:38 +0800

    add convenient sample

---
+++ boost/gbk_utf_sample/convenient/convenient_sample.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <boost/locale/encoding.hpp>
+
+char const*
+boost_converter(char const* text,char const* to_encoding,char const* from_encoding)
+{
+        return boost::locale::conv::between(text,to_encoding,from_encoding).c_str();
+}
+
+
+int main(){
+    //std::string temp="▒<9B><90>▒<84>";
+    std::string temp="\xD6""\xD0""\xCE""\xC4";
+    std::cout<<"sample chinese char is "<<temp<<std::endl;
+    auto temp_utf8 = boost_converter(temp.c_str(),"UTF-8","GBK");
+    std::cout<<"after convert1: "<<temp_utf8<<std::endl;
+
+    auto temp_gbk = boost_converter(temp_utf8,"GBK","UTF-8");
+    std::cout<<"after convert2: "<<temp_gbk<<std::endl;
+    return 0;
+}
