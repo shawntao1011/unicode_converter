df7ee485011f9a4b26d9b5ad7785e2bfc6a71dae
tao.sun <tao.sun@capitaledge.cn>
2021-05-27 14:32:22 +0800

    now all sytax is under c++ ,compatible for linux and windows

--- boost/gbk_utf_sample/dynamic_lib/lib/src/converter.cpp
+++ boost/gbk_utf_sample/dynamic_lib/lib/src/converter.cpp
@@ -1,10 +1,14 @@
 #include "converter.hpp"
-
+#include <algorithm>
 char const*
-API_DECL boost_converter(char const* text,char const* to_encoding,char const* from_encoding)
+API_DECL boost_converter(char const* text, char const* to_encoding, char const* from_encoding)
 {
-        std::string temp_str=boost::locale::conv::between(text,to_encoding,from_encoding);
-        char* ptr=new char[temp_str.size()+1];
-        strcpy_s(ptr,temp_str.size()+1,temp_str.c_str());
-        return ptr;
+    std::string temp_str = boost::locale::conv::between(text,to_encoding,from_encoding);
+    auto out = std::make_unique<char[]>(temp_str.size() + 1);
+    std::copy_n(temp_str.cbegin(), temp_str.size(), out.get());
+    return out.release();
+
+    // char* ptr=new char[temp_str.size()+1];
+    // strcpy_s(ptr,temp_str.size()+1,temp_str.c_str());
+    // return ptr;
 }
